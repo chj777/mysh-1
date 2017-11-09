bef736fd6337e200bacb4f3dd0f14e87169c77c7
chj777 <bookword@naver.com>
2017-11-09 19:41:52 +0900

    added process creation (trial:1)

--- src/commands.c
+++ src/commands.c
@@ -30,29 +30,78 @@ static int is_built_in_command(const char* command_name)
  */
 int evaluate_command(int n_commands, struct single_command (*commands)[512])
 {
+	
+  int pid;
+
   if (n_commands > 0) {
+
     struct single_command* com = (*commands);
 
     assert(com->argc != 0);
 
     int built_in_pos = is_built_in_command(com->argv[0]);
-    if (built_in_pos != -1) {
-      if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) {
-        if (built_in_commands[built_in_pos].command_do(com->argc, com->argv) != 0) {
-          fprintf(stderr, "%s: Error occurs\n", com->argv[0]);
-        }
-      } else {
+    if (built_in_pos != -1) 
+	{
+      
+	  if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) 
+	  {
+   
+		if (built_in_commands[built_in_pos].command_do(com->argc, com->argv) != 0) 
+		{
+			fprintf(stderr, "%s: Error occurs\n", com->argv[0]);       
+		}
+
+      } 
+	  
+	  else 
+	  {
         fprintf(stderr, "%s: Invalid arguments\n", com->argv[0]);
         return -1;
       }
-    } else if (strcmp(com->argv[0], "") == 0) {
+    
+	} 
+	
+	else if (strcmp(com->argv[0], "") == 0) 
+	{
       return 0;
-    } else if (strcmp(com->argv[0], "exit") == 0) {
+    } 
+	
+	else if (strcmp(com->argv[0], "exit") == 0) 
+	{
       return 1;
-    } else {
-      fprintf(stderr, "%s: command not found\n", com->argv[0]);
-      return -1;
-    }
+    } 
+	
+	else 
+	{
+      if(com->arg[0][0] == '/')
+	  {
+		pid = fork();
+
+		if(pid < 0)
+		{
+		//error
+			fprintf(stderr, "%s: Invalid arguments\n", com->argv[0]);
+		}
+		else if(pid == 0)
+		{
+		//childe process
+			execv(com->argv[0], com->argv);
+			exit(0);
+		}
+		else
+		{
+		//parendt process
+			wait(NULL);
+		}
+	  }
+	  
+	  else
+	  {
+	  	fprintf(stderr, "%s: command not found\n", com->argv[0]);
+      	return -1;
+	  }
+	}
+  
   }
 
   return 0;
